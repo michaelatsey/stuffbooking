3e0ee353ec852a342c1c443d9ea4b54c85b9c0c9
ATSE Michael (Prestataire) [IT-CE] <michael.atse@itgce.caisse-epargne.fr>
2020-09-01 17:31:43 +0200

    First commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+/**
+ * Class AppServiceProvider.
+ */
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Fix for MySQL < 5.7.7 and MariaDB < 10.2.2
+        // https://laravel.com/docs/master/migrations#creating-indexes
+        Schema::defaultStringLength(191);
+    }
+}
